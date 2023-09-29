8fc5e647d000b3ff1da2fff347d3271a06261889
bakorin <labFish893@gmail.com>
2023-09-29 23:50:15 +0900

    digitalizationを修正

--- app/Digitization.tsx
+++ app/Digitization.tsx
@@ -3,20 +3,24 @@ import Image from "next/image";
 export default function Digitization() {
   return (
     <section className="bg-white">
-      <div className="mx-auto grid max-w-screen-2xl grid-cols-12 gap-16 px-16 py-32">
-        <div className="md:col-span-6 hidden md:inline-block"></div>
-        <div className="flex ">
-          <Image
-            className=" self-end "
-            src={"/home/DigitizationImage.png"}
-            alt="Suspension analysis image"
-            width={500}
-            height={500}
-          />
+      <div className="mx-auto grid max-w-screen-2xl grid-cols-12 md:gap-16 px-16 py-32">
+        <div className="md:col-span-6 hidden md:inline-block">
+          <div className="flex ">
+            <Image
+              className=" self-end "
+              src={"/home/DigitizationImage.png"}
+              alt="Suspension analysis image"
+              width={500}
+              height={500}
+            />
+          </div>
         </div>
-        <div className="col-span-6">
-          <p className="text-4xl text-black">数字で見るNext FP</p>
-          <div className="mt-12 grid grid-cols-6 gap-y-16">
+
+        <div className="col-span-12 md:col-span-6 flex justify-center flex-col">
+          <p className="text-4xl text-black self-center md:self-start">
+            数字で見るNext FP
+          </p>
+          <div className="mt-12 grid grid-cols-6 gap-y-16 self-center md:self-start">
             <div className="col-span-4 md:col-span-2">
               <div className=" border border-white border-b-black text-black ">
                 <p
