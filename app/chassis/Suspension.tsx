184cb3613e177178593c37f05879e5c0c7b74eaf
gsx-r600 <boxfish893@gmail.com>
2023-09-29 15:44:05 +0900

    シャシーのページ作り途中

---
+++ app/chassis/Suspension.tsx
@@ -0,0 +1,16 @@
+export default function Suspension() {
+  return (
+    <section className="bg-white">
+      <div className="mx-auto max-w-screen-2xl  px-16 py-22">
+        <header className="flex flex-col">
+          <h2 className="text-6xl font-medium text-black self-center">
+            サスペンション
+          </h2>
+        </header>
+        <div className="mt-12">
+          <div className="flex flex-col gap-20"></div>
+        </div>
+      </div>
+    </section>
+  );
+}
