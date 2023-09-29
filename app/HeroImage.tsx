582826dd0526652e421e947128813dd60d1ad86e
bakorin <boxfish893@gmail.com>
2023-09-30 00:00:06 +0900

    break allで折り曲げることを学んだ

--- app/HeroImage.tsx
+++ app/HeroImage.tsx
@@ -11,7 +11,7 @@ export default function HeroImage() {
           <div className="grid w-full grid-cols-12">
             <div className="col-span-1"></div>
             <div className="col-span-8 mt-28 text-8xl font-bold">
-              <h1>
+              <h1 className="break-all">
                 Next Formula <br />
                 Project
               </h1>
