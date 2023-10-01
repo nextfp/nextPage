4d859b8ae2bfa43608ccc4ba6554b5ac1b349681
bakorin <labFish893@gmail.com>
2023-10-02 01:51:12 +0900

    スポンサー作りかけ

---
+++ app/sponsor/title.tsx
@@ -0,0 +1,17 @@
+export default function Title() {
+  return (
+    <section className="bg-white">
+      <div className="mx-auto max-w-screen-2xl  px-16 py-22">
+        <header className="flex flex-col justify-center">
+          <h1 className="self-center text-6xl font-medium text-black">
+            Next FPのスポンサー
+          </h1>
+          <h2 className="mt-24 self-center text-xl text-black">
+            私たちの活動はスポンサーなしでは成り立ちません。
+          </h2>
+        </header>
+        <div className="mt-12"></div>
+      </div>
+    </section>
+  );
+}
