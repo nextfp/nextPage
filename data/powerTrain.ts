697404a4e112565822f9dd9b7ef317b020290709
bakorin <boxfish893@gmail.com>
2023-10-01 17:14:49 +0900

    パワトレページの型完成

---
+++ data/powerTrain.ts
@@ -0,0 +1,17 @@
+const powerTrainList = [
+  {
+    title: "電装",
+    white: false,
+    element: [
+      {
+        name: "データローミング",
+        description:
+          "走行の度にエンジン回転数・水温・スロットル開閉度・GPSセンサー・空燃比を計測して、エンジンの燃調セッティングなどを煮詰めていく。",
+        imgSrc: "/powerTrain/MoteCLog.png",
+        imgAlt: "MoteC Log Image",
+      },
+    ],
+  },
+];
+
+export default powerTrainList;
