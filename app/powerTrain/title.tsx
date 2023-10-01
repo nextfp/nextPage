ceb0819b3362d0499df39e0e3a03736606b7b4ec
bakorin <labFish893@gmail.com>
2023-10-01 17:14:49 +0900

    パワトレページの型完成

---
+++ app/powerTrain/title.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import powerTrainImage from "@/public/powerTrain/powerTrainImage.png";
+
+export default function Title() {
+  return (
+    <section className="bg-white">
+      <div className="mx-auto max-w-screen-2xl  px-16 py-22">
+        <header className="flex flex-col justify-center">
+          <h1 className="text-6xl font-medium text-black self-center">
+            強力なパワートレイン
+          </h1>
+          <Image
+            className="self-center"
+            src={powerTrainImage}
+            alt="Power train Cad ASSY"
+          />
+        </header>
+        <div className="mt-12">
+          <div className="flex flex-col gap-20"></div>
+        </div>
+      </div>
+    </section>
+  );
+}
