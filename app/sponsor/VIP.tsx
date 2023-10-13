74dbf970fbdd8f6fafa9cd0429a7663db40b94e4
nextfp <nextformula038@gmail.com>
2023-10-13 19:37:08 +0900

    ホバーを追加

--- app/sponsor/VIP.tsx
+++ app/sponsor/VIP.tsx
@@ -11,7 +11,7 @@ const VIP = () => {
           <div key={index}>
             <SponsorPopUp data={data}>
               <section
-                className="rounded-2xl bg-cover bg-center bg-no-repeat"
+                className="rounded-2xl bg-cover bg-center bg-no-repeat hover:scale-105"
                 style={{ backgroundImage: "url(" + data.bgImage + ")" }}
                 key={index}
               >
@@ -25,7 +25,7 @@ const VIP = () => {
                     <div className="grid grid-cols-12 gap-4 gap-y-16 md:gap-12">
                       <div className="col-span-full  lg:col-span-5">
                         <Image
-                          className="p-3 rounded-3xl bg-white mx-auto"
+                          className="mx-auto rounded-3xl bg-white p-3"
                           src={data.Image}
                           alt={data.ImageAlt}
                           width={633 * 0.8}
@@ -33,7 +33,7 @@ const VIP = () => {
                         />
                       </div>
 
-                      <div className="col-span-full flex flex-col gap-6 justify-center lg:col-span-5 lg:col-start-7">
+                      <div className="col-span-full flex flex-col justify-center gap-6 lg:col-span-5 lg:col-start-7">
                         <p className="self-center break-all text-xl text-white lg:self-start">
                           {data.description.split("\n").map((line, index) => (
                             <React.Fragment key={index}>
