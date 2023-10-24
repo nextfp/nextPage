daaf4eaadff16245445066d9217c5dc1846544d0
bakorin <labFish893@gmail.com>
2023-10-25 01:05:23 +0900

    feat: sponsorBenefit内の背景を白に変更

--- app/sponsor/detail/PR.tsx
+++ app/sponsor/detail/PR.tsx
@@ -1,8 +1,10 @@
 const PR = () => {
   return (
     <>
-      <h3 className="text-3xl font-medium">2.各種SNSにおける広報活動</h3>
-      <p>
+      <h3 className="text-3xl font-medium text-black">
+        2.各種SNSにおける広報活動
+      </h3>
+      <p className="text-black">
         当チームで運営しているSNS(XやInstagram)において、ご支援いただいた際に以下の写真のような形で、提供物及び企業様の説明文を掲載させていただきます。
         <br />
         <br />
--- app/sponsor/detail/sticker.tsx
+++ app/sponsor/detail/sticker.tsx
@@ -4,8 +4,10 @@ import SponsorSticker from "@/data/sponsorSticker";
 const Sticker = () => {
   return (
     <>
-      <h3 className="text-3xl font-medium">1.車体へのステッカー掲載</h3>
-      <p>
+      <h3 className="text-3xl font-medium text-black">
+        1.車体へのステッカー掲載
+      </h3>
+      <p className="text-black">
         スポンサー様から頂いた資金、技術物品支援を見積もった金額を考慮し金額相当で車体の掲載場所や大きさをこちらで決定させていただきます。
         <br />
         <br />
@@ -14,8 +16,8 @@ const Sticker = () => {
       <div className="grid grid-cols-2 gap-8">
         {SponsorSticker.map((data, index) => (
           <div key={index} className="col-span-full md:col-span-1">
-            <div className="rounded-3xl border-2 border-white bg-black">
-              <div className="rounded-t-2xl bg-white">
+            <div className="rounded-3xl border-2 border-black bg-white">
+              <div className="rounded-t-2xl bg-black">
                 <Image
                   className="mx-auto py-8"
                   src={data.img}
@@ -25,8 +27,8 @@ const Sticker = () => {
                 />
               </div>
               <div className="flex flex-col gap-8 p-8">
-                <h4 className="text-2xl">{data.title}</h4>
-                <p className="">{data.description}</p>
+                <h4 className="text-3xl font-bold text-black">{data.title}</h4>
+                <p className="text-black">{data.description}</p>
               </div>
             </div>
           </div>
