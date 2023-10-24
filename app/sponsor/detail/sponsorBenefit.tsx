c7e3e6265766ce8ed841236016665f0e219da312
bakorin <boxfish893@gmail.com>
2023-10-25 01:05:23 +0900

    feat: sponsorBenefit内の背景を白に変更

--- app/sponsor/detail/sponsorBenefit.tsx
+++ app/sponsor/detail/sponsorBenefit.tsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const SponsorBenefit = ({ children }: { children: React.ReactNode }) => {
   return (
-    <section className="bg-black">
+    <section className="bg-white">
       <div className="mx-auto max-w-screen-2xl px-8 py-22 flex flex-col gap-8 sm:px-28 border border-t-white border-black">
         <header className="mx-auto">
-          <h2 className="mb-8 text-4xl font-medium  self-center">
+          <h2 className="mb-8 text-4xl font-medium self-center text-black">
             スポンサー様の特典内容
           </h2>
         </header>
