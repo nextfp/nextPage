b9110a4602cf31d3aab3b9d7d7d93e757f4ebabd
next-fp <nextformula038@gmail.com>
2023-10-25 13:31:41 +0900

    feat: ロゴ掲載場所の写真を追加

--- data/sponsorSticker.ts
+++ data/sponsorSticker.ts
@@ -3,25 +3,25 @@ const SponsorSticker = [
     title: "5万円相当",
     description:
       "マシンの側面に掲載。スペースの関係上見えづらい可能性があります。",
-    img: "",
+    img: "/sponsor/LogoSpace5.webp",
     imgAlt: "",
   },
   {
     title: "15万円相当",
     description: "マシンの正面やマシンの側面に少し目立つ形で掲載。",
-    img: "",
+    img: "/sponsor/LogoSpace15.webp",
     imgAlt: "",
   },
   {
     title: "30万円相当",
     description: "マシンの正面やマシンの側面によく目立つ形で掲載。",
-    img: "",
+    img: "/sponsor/logoSpace30.webp",
     imgAlt: "",
   },
   {
     title: "50万円相当",
     description: "マシンの正面から走行中もよく見える形で掲載。",
-    img: "",
+    img: "/sponsor/LogoSpace40.webp",
     imgAlt: "",
   },
 ];
