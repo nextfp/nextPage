af8f06db6fe13331866054d2c300fe36780c4c63
nextfp <nextformula038@gmail.com>
2023-11-27 19:07:33 +0900

    feat: metaタグを追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Noto_Sans_JP } from "next/font/google";
+import siteMetadata from "@/data/siteMetadata";
 
 const noto = Noto_Sans_JP({
   weight: ["400", "500", "700"],
@@ -8,13 +9,39 @@ const noto = Noto_Sans_JP({
   display: "swap",
 });
 
+export const metadata: Metadata = {
+  metadataBase: new URL(siteMetadata.siteUrl),
+  title: {
+    default: siteMetadata.title,
+    template: `%s | ${siteMetadata.title}`,
+  },
+  description: siteMetadata.description,
+  openGraph: {
+    title: siteMetadata.title,
+    description: siteMetadata.description,
+    url: "./",
+    siteName: siteMetadata.title,
+    images: [siteMetadata.socialBanner],
+    locale: "JP",
+    type: "website",
+  },
+  twitter: {
+    site: siteMetadata.title,
+    card: "summary_large_image",
+    images: [siteMetadata.socialBanner],
+  },
+};
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="ja">
+    <html lang={siteMetadata.language}>
+      <link rel="apple-touch-icon" sizes="76x76" href="/nextFP.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/nextFP.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/nextFP.png" />
       <body className={noto.className}>{children}</body>
     </html>
   );
---
+++ data/siteMetadata.ts
@@ -0,0 +1,38 @@
+const siteMetadata = {
+  title: "新潟大学学生フォーミュラプロジェクト",
+  author: "next formula project sponsor staff",
+  headerTitle: "Next FP",
+  description: "新潟大学 NEXT学生フォーミュラプロジェクトのサイトです。",
+  language: "ja",
+  theme: "system", // system, dark or light
+  siteUrl: "https://next-fp.com/",
+  siteRepo: "https://github.com/nextfp/nextPage",
+  siteLogo: "/nextFP.png",
+  socialBanner: "/twitter-card.webp",
+  email: "next-fp@eng.niigata-u.ac.jp",
+  github: "https://github.com/nextfp",
+  twitter: "https://twitter.com/NEXT_Formula",
+  youtube: "https://www.youtube.com/channel/UC4didWGeB9XZ70Q3JeKMtTg",
+  locale: "ja",
+  analytics: {
+    // If you want to use an analytics provider you have to add it to the
+    // content security policy in the `next.config.js` file.
+    // supports Plausible, Simple Analytics, Umami, Posthog or Google Analytics.
+    //umamiAnalytics: {
+    // We use an env variable for this site to avoid other users cloning our analytics ID
+    // umamiWebsiteId: process.env.NEXT_UMAMI_ID, // e.g. 123e4567-e89b-12d3-a456-426614174000
+    //},
+    // plausibleAnalytics: {
+    //   plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
+    // },
+    // simpleAnalytics: {},
+    // posthogAnalytics: {
+    //   posthogProjectApiKey: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
+    // },
+    // googleAnalytics: {
+    //   googleAnalyticsId: '', // e.g. G-XXXXXXX
+    // },
+  },
+};
+
+export default siteMetadata;
