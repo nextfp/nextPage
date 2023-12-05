eb1dc3678da1469ef1a086d7e7a9536d3e89aa1e
boxfish <labFish893@gmail.com>
2023-12-06 01:25:39 +0900

    feat: metaタグをurlごとにtitleが変わるようにした

--- app/powerTrain/page.tsx
+++ app/powerTrain/page.tsx
@@ -3,6 +3,15 @@ import Header from "@/components/Header";
 import Main from "./main";
 import Footer from "@/components/Footer";
 
+export const metadata = {
+  title: "パワートレイン",
+  description:
+    "私たちのチームでは、大きく分けると2つのパートに分かれるのですが、そのうちのパワートレインパートの説明です。吸気、排気、電装、燃料、冷却、駆動といったパートがここに含まれます。どのパートも何年もの間引き継がれてきた経験をもとに、工夫を凝らした設計を毎年マシンに反映させています。",
+  openGraph: {
+    title: "パワートレイン",
+  },
+};
+
 export default function Home() {
   return (
     <div className="bg-white">
