30738c29d0d9f35e614c354a53689f1a0eddfdc0
tjcpyh03 <nextformula038@gmail.com>
2023-12-08 18:36:47 +0900

    refactor: スポンサーのliteで使用しているcardをコンポーネント化

---
+++ components/card.tsx
@@ -0,0 +1,43 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const Card = ({
+  title,
+  description,
+  image,
+  imageAlt,
+  imageSize,
+  src,
+}: {
+  title: string;
+  description: string;
+  image: string;
+  imageAlt: string;
+  imageSize: number;
+  src: URL;
+}) => {
+  return (
+    <Link href={src} className="cursor-pointer">
+      <div className="h-full rounded-3xl border-2 border-white bg-black hover:scale-105">
+        <div className="h-[150px] rounded-t-2xl bg-white">
+          <div className="flex h-full justify-center">
+            <Image
+              className="m-auto h-fit"
+              src={image}
+              alt={imageAlt}
+              width={633 * imageSize}
+              height={415 * imageSize}
+            />
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-8 p-8">
+          <h4 className="text-2xl">{title}</h4>
+          <p className="">{description}</p>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default Card;
