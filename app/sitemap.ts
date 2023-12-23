260d3758342b0f08fabcfcb0fbd833ec3717ae94
4900yen <boxfish893@gmail.com>
2023-12-23 21:21:22 +0900

    fix: sitemapを自動生成されるように

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,16 +1,23 @@
 import { MetadataRoute } from "next";
 import siteMetadata from "@/data/siteMetadata";
+import headerNavLinks from "@/data/headerNavLinks";
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseURL = siteMetadata.siteUrl;
-  const lastModified = new Date();
+  const lastModified: Date = new Date();
+  let staticPaths: { url: string; lastModified: Date }[] = [];
 
-  const staticPaths = [
-    {
-      url: baseURL,
+  headerNavLinks.forEach((link) => {
+    staticPaths.push({
+      url: baseURL + link.href,
       lastModified,
-    },
-  ];
+    });
+  });
+
+  staticPaths.push({
+    url: baseURL + "/sponsor/detail",
+    lastModified,
+  });
 
   return [...staticPaths];
 }
