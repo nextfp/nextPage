3bfc7b8f4c47192fda3f733b1fd78bcffdd96956
labfish <168062620+labFish00@users.noreply.github.com>
2026-02-04 13:34:46 +0900

    chore: バージョン上げ

--- next.config.js
+++ next.config.js
@@ -1,15 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: [
-        {
-          loader: "@svgr/webpack",
-        },
-      ],
-    });
-    return config;
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: [
+          {
+            loader: '@svgr/webpack',
+            options: {
+              icon: true,
+            },
+          },
+        ],
+        as: '*.js',
+      },
+    },
   },
 };
 
